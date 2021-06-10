843be00f88bc83015c1f991d679b8e97bace20b2
TrongHoang21 <drawk0129@gmail.com>
2021-06-11 04:11:09 +0700

    tái sử dụng file config thành công, việc cần làm tiếp theo là xử lí các mqh 1n, và implement DAO CRUDSS tất cả các file còn lại. Sau đó import lên swing

--- src/HibernateArtifacts/HibernateUtil.java
+++ src/HibernateArtifacts/HibernateUtil.java
@@ -16,29 +16,36 @@ public class HibernateUtil {
     private static SessionFactory sessionFactory ;
     static {
         try {
-                       Configuration configuration = new Configuration();
+            //Configuration configuration = new Configuration();
 
              //Hibernate settings equivalent to hibernate.cfg.xml's properties
-            Properties settings = new Properties();
-            settings.put(Environment.DRIVER, "com.mysql.jdbc.Driver");
-            settings.put(Environment.URL, "jdbc:mysql://localhost:3306/Hibernate?characterEncoding=UTF-8");
-            settings.put(Environment.USER, "hoang");
-            settings.put(Environment.PASS, "laptrinhjava");
-            settings.put(Environment.DIALECT, "org.hibernate.dialect.MySQL5Dialect");
+//            Properties settings = new Properties();
+//            settings.put(Environment.DRIVER, "com.mysql.jdbc.Driver");
+//            settings.put(Environment.URL, "jdbc:mysql://localhost:3306/Hibernate?characterEncoding=UTF-8");
+//            settings.put(Environment.USER, "hoang");
+//            settings.put(Environment.PASS, "laptrinhjava");
+//            settings.put(Environment.DIALECT, "org.hibernate.dialect.MySQL5Dialect");
+//
+//            settings.put(Environment.SHOW_SQL, "true");
+//
+//            settings.put(Environment.CURRENT_SESSION_CONTEXT_CLASS, "thread");
+//
+//            settings.put(Environment.HBM2DDL_AUTO, "create-drop");
+//
+//            configuration.setProperties(settings);
+//
+//            configuration.addAnnotatedClass(Classroom.class);
+//
+//            ServiceRegistry serviceRegistry = new StandardServiceRegistryBuilder().applySettings(configuration.getProperties()).build();
 
-            settings.put(Environment.SHOW_SQL, "true");
+      //      sessionFactory = configuration.buildSessionFactory(serviceRegistry);
 
-            settings.put(Environment.CURRENT_SESSION_CONTEXT_CLASS, "thread");
-
-            settings.put(Environment.HBM2DDL_AUTO, "create-drop");
-
-            configuration.setProperties(settings);
-
-            configuration.addAnnotatedClass(Classroom.class);
-
-            ServiceRegistry serviceRegistry = new StandardServiceRegistryBuilder().applySettings(configuration.getProperties()).build();
-
-            sessionFactory = configuration.buildSessionFactory(serviceRegistry);
+            try {
+                Configuration configuration = new Configuration();
+                sessionFactory = configuration.configure().buildSessionFactory();
+            } catch (Throwable ex) {
+                ex.printStackTrace();
+            }
 
         } catch (Throwable ex) {
             System.err.println("Initial SessionFactory creation failed." + ex);
--- src/HibernateProject/main.java
+++ src/HibernateProject/main.java
@@ -2,23 +2,25 @@ package HibernateProject;
 import java.util.*;
 
 import DAO.ClassroomDAO;
+import DAO.SinhVienAccountDAO;
 import POJOs.Classroom;
+import POJOs.SinhVienAccount;
 
 public class main {
     public static void main(String[] args) {
 
-//        List<SinhVienAccount> ds= SinhVienAccountDAO.layDanhSachSinhVien();
-//        for(int i=0; i<ds.size(); i++){
-//            SinhVienAccount sv=ds.get(i);
-//            System.out.println("MSSV: "+sv.getMaSinhVien());
-//            System.out.println("Họ và tên: "+sv.getHoTen());
-//        }
-
-        List<Classroom> ds= ClassroomDAO.layDanhSachLop();
+        List<SinhVienAccount> ds= SinhVienAccountDAO.layDanhSachSinhVien();
         for(int i=0; i<ds.size(); i++){
-            Classroom sv= ds.get(i);
-            System.out.println("MSSV: "+sv.getMaLop());
-            System.out.println("Họ và tên: "+sv.getTenLop());
+            SinhVienAccount sv=ds.get(i);
+            System.out.println("MSSV: "+sv.getMaSinhVien());
+            System.out.println("Họ và tên: "+sv.getHoTen());
         }
+
+//        List<Classroom> ds= ClassroomDAO.layDanhSachLop();
+//        for(int i=0; i<ds.size(); i++){
+//            Classroom sv= ds.get(i);
+//            System.out.println("MSSV: "+sv.getMaLop());
+//            System.out.println("Họ và tên: "+sv.getTenLop());
+//        }
     }
 }
